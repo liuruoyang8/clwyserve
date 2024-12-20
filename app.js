5d54ed6beee91b284517c327d2c1e1f683a764bc
liuruoyang <572772285@qq.com>
2024-12-20 09:36:45 +0800

    2024-12-20

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const logger = require('morgan');
 //引入环境变量包
 require('dotenv').config();
 const adminAuth = require('./middlewares/admin-auth');
-// const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const userAuth = require('./middlewares/user-auth');
 //文章列表接口
 const articlesAdminRouter=require('./routes/admin/articles');
 //分类列表
@@ -23,11 +24,21 @@ const adminChaptersRouter = require('./routes/admin/chapters');
 const adminChartsRouter = require('./routes/admin/charts');
 //用户登陆
 const adminAuthRouter = require('./routes/admin/auth');
-
-// ...
-
-
-// ...
+// const userAuth = require('./middlewares/user-auth');
+// 前端展示分类接口
+const categoriesRouter = require('./routes/categories');
+// 前端展示课程接口
+const coursesRouter = require('./routes/courses');
+// 前端章节接口
+const chaptersRouter = require('./routes/chapters');
+// 前端文章接口
+const articlesRouter = require('./routes/articles');
+// 前端系统信息接口
+const settingsRouter = require('./routes/settings');
+// 前端搜索接口
+const searchRouter = require('./routes/search');
+// 前端注册接口
+const authRouter = require('./routes/auth');
 
 
 const app = express();
@@ -53,7 +64,8 @@ app.use(function(req,res,next){
     // **************上面的代码一写那么就等于这个服务器开启了跨域资源共享
     next()
 })
-
+//中间件配置
+app.use('/users', userAuth, usersRouter);
 // 后台路由配置
 app.use('/admin/articles', adminAuth, articlesAdminRouter);
 app.use('/admin/categories', adminAuth, adminCategoriesRouter);
@@ -62,6 +74,16 @@ app.use('/admin/users', adminAuth, adminUsersRouter);
 app.use('/admin/courses', adminAuth, adminCoursesRouter);
 app.use('/admin/chapters', adminAuth, adminChaptersRouter);
 app.use('/admin/charts', adminAuth, adminChartsRouter);
+//中间件配置
 app.use('/admin/auth', adminAuthRouter);
+//前端展示路由
+app.use('/chapters', chaptersRouter);
+app.use('/courses', coursesRouter);
+app.use('/articles', articlesRouter);
+app.use('/settings', settingsRouter);
+app.use('/search', searchRouter);
+app.use('/categories', categoriesRouter);
+app.use('/', indexRouter);
+app.use('/auth', authRouter);
 
 module.exports = app;
